01fc3696320b6fd515f928a3947a22f7c05173eb
Nicolas De Maio <nicolas.demaio19@gmail.com>
2022-08-24 23:14:56 -0300

    Initial commit

---
+++ src/main/java/ar/edu/unq/desapp/grupof/backendcriptop2papi/BackendCriptop2pApiApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.desapp.grupof.backendcriptop2papi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCriptop2pApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCriptop2pApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unq/desapp/grupof/backendcriptop2papi/BackendCriptop2pApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.desapp.grupof.backendcriptop2papi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCriptop2pApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
