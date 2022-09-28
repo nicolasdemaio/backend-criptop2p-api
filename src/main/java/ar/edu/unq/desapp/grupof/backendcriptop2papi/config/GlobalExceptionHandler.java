23b5e8e5f5a46334c707a362044517dd2df6ab10
Nicolas De Maio <nicolas.demaio19@gmail.com>
2022-09-27 23:19:39 -0300

    Removed class without tests

--- src/main/java/ar/edu/unq/desapp/grupof/backendcriptop2papi/config/GlobalExceptionHandler.java
+++ src/main/java/ar/edu/unq/desapp/grupof/backendcriptop2papi/config/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import ar.edu.unq.desapp.grupof.backendcriptop2papi.model.exceptions.EmailAlread
 import ar.edu.unq.desapp.grupof.backendcriptop2papi.utils.ApiMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -29,14 +31,14 @@ public class GlobalExceptionHandler {
         return new ApiMessage().response(HttpStatus.BAD_REQUEST, error);
     }
 
-    /*@ExceptionHandler(MethodArgumentNotValidException.class)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     ResponseEntity<Map<String, Object>> onMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         Map<String, String> error = new HashMap<>();
         for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
             error.put(fieldError.getField(), fieldError.getDefaultMessage());
         }
         return new ApiMessage().response(HttpStatus.BAD_REQUEST, error);
-    }*/
+    }
 
 }
 
