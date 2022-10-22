6473fe552bfa16c700c397c2b1334cb1babed53d
TrejoJulian <trejojulian998@gmail.com>
2022-10-22 00:59:06 -0300

    Smell fixes

--- src/main/java/ar/edu/unq/desapp/grupof/backendcriptop2papi/webservice/AuthController.java
+++ src/main/java/ar/edu/unq/desapp/grupof/backendcriptop2papi/webservice/AuthController.java
@@ -10,7 +10,6 @@ import ar.edu.unq.desapp.grupof.backendcriptop2papi.service.InvestorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
