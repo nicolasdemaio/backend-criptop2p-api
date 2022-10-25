babea459d675f973cdf07b9343706020eb0f506a
Nicolas De Maio <nicolas.demaio19@gmail.com>
2022-10-25 00:18:57 -0300

    added test for Statistics service

--- src/test/java/ar/edu/unq/desapp/grupof/backendcriptop2papi/service/TradeStatiticsServiceTest.java
+++ src/test/java/ar/edu/unq/desapp/grupof/backendcriptop2papi/service/TradeStatiticsServiceTest.java
@@ -2,7 +2,10 @@ package ar.edu.unq.desapp.grupof.backendcriptop2papi.service;
 
 import ar.edu.unq.desapp.grupof.backendcriptop2papi.dto.AssetStatistic;
 import ar.edu.unq.desapp.grupof.backendcriptop2papi.dto.InvestorStatistic;
+import ar.edu.unq.desapp.grupof.backendcriptop2papi.model.CryptoCurrency;
+import ar.edu.unq.desapp.grupof.backendcriptop2papi.model.CryptoQuotation;
 import ar.edu.unq.desapp.grupof.backendcriptop2papi.model.InvestmentAccount;
+import ar.edu.unq.desapp.grupof.backendcriptop2papi.model.MarketOrder;
 import ar.edu.unq.desapp.grupof.backendcriptop2papi.model.exceptions.InvestorNotFoundException;
 import ar.edu.unq.desapp.grupof.backendcriptop2papi.model.operation.Operation;
 import ar.edu.unq.desapp.grupof.backendcriptop2papi.persistence.InvestmentAccountRepository;
@@ -16,16 +19,19 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class TradeStatiticsServiceTest {
+class TradeStatisticsServiceTest {
 
     @Mock
     private InvestmentAccountRepository investmentAccountRepository;
+
+    @Mock
+    private QuotationService quotationService;
 
     @InjectMocks
     private TradeStatisticsService tradeStatisticsService;
@@ -38,7 +44,7 @@ public class TradeStatiticsServiceTest {
     }
 
     @Test
-    void testGetTradeStatiticsOfAccount() {
+    void testGetTradeStatisticsOfAccount() {
         List<Operation> operations = List.of();
         InvestmentAccount account = Mockito.mock(InvestmentAccount.class);
         when(account.getOperations()).thenReturn(operations);
@@ -51,6 +57,45 @@ public class TradeStatiticsServiceTest {
         assertThat(investorStatistic.getTotalQuantityInDollars()).isZero();
         assertThat(investorStatistic.getTotalQuantityInPesos()).isZero();
         assertThat(investorStatistic.getTimestamp()).isNotNull();
+    }
+
+    @Test
+    void testGetTradeStatisticsOfAccountWith1CompletedOperation() {
+        Operation operation = getMockedOperation();
+
+        List<Operation> operations = List.of(operation);
+        InvestmentAccount account = Mockito.mock(InvestmentAccount.class);
+        when(account.getOperations()).thenReturn(operations);
+        when(investmentAccountRepository.findInvestmentAccountByInvestor(1L)).thenReturn(account);
+
+        InvestorStatistic investorStatistic =
+                tradeStatisticsService.getStatisticsFrom(1L, LocalDateTime.now(), LocalDateTime.now());
+
+        assertThat(investorStatistic.getAssetStatistics()).isNotEmpty();
+        assertThat(investorStatistic.getTotalQuantityInDollars()).isOne();
+        assertThat(investorStatistic.getTotalQuantityInPesos()).isOne();
+        assertThat(investorStatistic.getTimestamp()).isNotNull();
+    }
+
+    private Operation getMockedOperation() {
+        var order = Mockito.mock(MarketOrder.class);
+        var cryptoQuotation = Mockito.mock(CryptoQuotation.class);
+
+        when(cryptoQuotation.getPriceInDollars()).thenReturn(1d);
+        when(cryptoQuotation.getPriceInPesos()).thenReturn(1d);
+        when(cryptoQuotation.getCryptoCurrency()).thenReturn(CryptoCurrency.AAVEUSDT);
+
+        Operation operation = Mockito.mock(Operation.class);
+        when(operation.wasOriginatedBetween(any(), any())).thenReturn(true);
+        when(operation.isCompleted()).thenReturn(true);
+
+        when(order.getNominalQuantity()).thenReturn(1d);
+
+        when(operation.getSourceOfOrigin()).thenReturn(order);
+        when(operation.getCryptoQuotation()).thenReturn(cryptoQuotation);
+        when(quotationService.getCryptoQuotation(CryptoCurrency.AAVEUSDT))
+                .thenReturn(cryptoQuotation);
+        return operation;
     }
 
 }
