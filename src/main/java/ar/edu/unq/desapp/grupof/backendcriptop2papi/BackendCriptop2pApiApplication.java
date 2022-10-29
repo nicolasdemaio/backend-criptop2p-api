80515137b63906d67c656f4392e9ca24c326ce93
Nicolas De Maio <nicolas.demaio19@gmail.com>
2022-10-28 23:36:30 -0300

    feature: cache scaffolding - continue implementation

--- src/main/java/ar/edu/unq/desapp/grupof/backendcriptop2papi/BackendCriptop2pApiApplication.java
+++ src/main/java/ar/edu/unq/desapp/grupof/backendcriptop2papi/BackendCriptop2pApiApplication.java
@@ -4,9 +4,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableScheduling
 public class BackendCriptop2pApiApplication {
 
 	public static void main(String[] args) {
