c451ba1103a94ddb5ce06725d827fa71edef4dfc
TrejoJulian <trejojulian998@gmail.com>
2022-11-12 17:35:31 -0300

    ADDED DTO And general coding rules tests

---
+++ src/test/java/ar/edu/unq/desapp/grupof/backendcriptop2papi/architecture/GeneralCodingRulesTest.java
@@ -0,0 +1,18 @@
+package ar.edu.unq.desapp.grupof.backendcriptop2papi.architecture;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.library.GeneralCodingRules.*;
+
+@AnalyzeClasses(packages = "ar/edu/unq/desapp/grupof/backendcriptop2papi")
+public class GeneralCodingRulesTest {
+
+    @ArchTest
+    private final ArchRule no_generic_exceptions = NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS;
+
+    @ArchTest
+    private final ArchRule no_java_util_logging = NO_CLASSES_SHOULD_USE_JAVA_UTIL_LOGGING;
+
+}
